642995bc890b5eddf5aad25f32a0f22709f7b171
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-15 13:05:08 +0100

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -21,7 +21,7 @@ class UnitTests(unittest.TestCase) :
     def test_variable(self) : 
        inputs, variables = [], []
        
-       mat1 = np.array([[0.3,0.5,0.2],[0.3,0.4,0.3],[0.2,0.5,0.3]])
+       myvars, mat1 = np.array([0,1,2]), np.array([[0.3,0.5,0.2],[0.3,0.4,0.3],[0.2,0.5,0.3]])
        for j in range(3) : 
            exp = np.dot( mat1[j,:], myvars )
            var = var = np.dot( mat1[j,:], myvars*myvars ) - exp*exp
@@ -29,20 +29,20 @@ class UnitTests(unittest.TestCase) :
                inputs.append((mat1,j,))
                myvar = randomvar( exp, variance=var, vmin=0, vmax=2, isinteger=True )
                variables.append( myvar )
-       mat2 = np.array([[1,0,0,0,0],[1/3,1/3,1/3,0,0],[0,0.5,0,0.5,0],[0,0.5,0,0,0.5],[0,0,0,0,1]])
-       for j in range(3) : 
+       myvars, mat2 = np.array([0,1,2,3,4]), np.array([[1,0,0,0,0],[1/3,1/3,1/3,0,0],[0,0.5,0,0.5,0],[0,0.5,0,0,0.5],[0,0,0,0,1]])
+       for j in range(5) : 
            exp = np.dot( mat2[j,:], myvars )
            var = var = np.dot( mat2[j,:], myvars*myvars ) - exp*exp
            for i in range(10): 
                inputs.append((mat2,j,))
-               myvar = randomvar( exp, variance=var, vmin=0, vmax=2, isinteger=True )
+               myvar = randomvar( exp, variance=var, vmin=0, vmax=4, isinteger=True )
                variables.append( myvar ) 
 
        assert( check_func("markov_move", inputs, variables ) )
 
     def test_mean(self) : 
        inputs, variables = [], []
-       mat1 = np.array([[0.3,0.5,0.2],[0.3,0.4,0.3],[0.2,0.5,0.3]])
+       myvars, mat1 = np.array([0,1,2]), np.array([[0.3,0.5,0.2],[0.3,0.4,0.3],[0.2,0.5,0.3]])
        for j in range(3) : 
            exp = np.dot( mat1[j,:], myvars )
            var = var = np.dot( mat1[j,:], myvars*myvars ) - exp*exp
