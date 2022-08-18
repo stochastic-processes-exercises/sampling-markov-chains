ef52b18a0107966bbfc5ac774b680c11995733cc
Gareth Tribello <gareth.tribello@gmail.com>
2022-08-18 13:02:41 +0100

    Fixed test to work with new autofeedback

--- testing/test_main.py
+++ testing/test_main.py
@@ -11,12 +11,6 @@ from AutoFeedback.randomclass import randomvar
 import unittest
 from main import *
 
-class helper : 
-    def check_mean( n, probs, start ) :
-        m = 0
-        for i in range(n) : m = m + markov_move( probs, start ) 
-        return m / n
-
 class UnitTests(unittest.TestCase) :
     def test_variable(self) : 
        inputs, variables = [], []
@@ -27,7 +21,7 @@ class UnitTests(unittest.TestCase) :
            var = var = np.dot( mat1[j,:], myvars*myvars ) - exp*exp
            for i in range(10): 
                inputs.append((mat1,j,))
-               myvar = randomvar( exp, variance=var, vmin=0, vmax=2, isinteger=True )
+               myvar = randomvar( exp, variance=var, vmin=0, vmax=2, isinteger=True, nsamples=100 )
                variables.append( myvar )
        myvars, mat2 = np.array([0,1,2,3,4]), np.array([[1,0,0,0,0],[1/3,1/3,1/3,0,0],[0,0.5,0,0.5,0],[0,0.5,0,0,0.5],[0,0,0,0,1]])
        for j in range(5) : 
@@ -35,21 +29,7 @@ class UnitTests(unittest.TestCase) :
            var = var = np.dot( mat2[j,:], myvars*myvars ) - exp*exp
            for i in range(10): 
                inputs.append((mat2,j,))
-               myvar = randomvar( exp, variance=var, vmin=0, vmax=4, isinteger=True )
+               myvar = randomvar( exp, variance=var, vmin=0, vmax=4, isinteger=True, nsamples=100 )
                variables.append( myvar ) 
 
        assert( check_func("markov_move", inputs, variables ) )
-
-    def test_mean(self) : 
-       inputs, variables = [], []
-       myvars, mat1 = np.array([0,1,2]), np.array([[0.3,0.5,0.2],[0.3,0.4,0.3],[0.2,0.5,0.3]])
-       for j in range(3) : 
-           exp = np.dot( mat1[j,:], myvars )
-           var = var = np.dot( mat1[j,:], myvars*myvars ) - exp*exp
-           for i in range(5):  
-               nmean = (i+1)*10
-               inputs.append((nmean,mat1,j))
-               myvar = randomvar( exp, variance=var/nmean, vmin=0, vmax=4, isinteger=False )
-               variables.append( myvar )
-
-       assert( check_func("check_mean", inputs, variables, modname=helper ) )
